1c416bd900a6f45ac573c6fae62339378edf74e9
Abhishek Marathe <abmarath@microsoft.com>
2019-08-28 11:07:16 -0700

    checking subprocess in python

--- GetGitLog.py
+++ GetGitLog.py
@@ -1,5 +1,20 @@
-import git
+import subprocess
+import os
+# import git
 
-repo = git.Git('~/PatchTracker/linux')
-logInfo = repo.heads.master.log()
-print(logInfo)
+# repo = git.Git('~/PatchTracker/linux')
+# logInfo = repo.heads.master.log()
+# print(logInfo)
+currDir = os.getcwd()
+with open ('hyperVfiles.txt','r') as fileList:
+    os.chdir("../linux")
+    for hyperVfile in fileList:
+        #myCommand = os.popen("git log -p -- "+hyperVfile).read()
+        commits = subprocess.Popen(['git log','-p','--',hyperVfile],
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT)
+        stdout, stderr = commits.communicate()
+        print(stdout)
+        print("error "+stderr)
+
+fileList.closed
