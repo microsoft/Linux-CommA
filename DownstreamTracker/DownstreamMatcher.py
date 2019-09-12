9e1183e59f194a205f8d894b549eb58ad19730eb
Ryan Feldman <ryfeldma@microsoft.com>
2019-09-11 17:01:42 -0700

    first pass of DownstreamMatcher setup to find best matching upstream patch

--- DownstreamTracker/DownstreamMatcher.py
+++ DownstreamTracker/DownstreamMatcher.py
@@ -1,37 +1,74 @@
 
+from fuzzywuzzy import fuzz
+from Objects.DistroPatchMatch import DistroPatchMatch
+
 class DownstreamMatcher:
     
-    def __init__(upstream_patches):
+    def __init__(self, upstream_patches):
         """
         Creates the DownstreamMatcher.
-        upstream_patches is a list of Patch objects from upstream we will check against
+        upstream_patches is a dict in the form {patch_id : Patch}
         """
         self.upstream_patches = upstream_patches
 
 
 
-    def get_matching_patch(downstream_patch):
+    def get_matching_patch(self, downstream_patch):
         """
         downstream_patch is a Patch object to match to upstream
-        Returns: DistroPatchMatch, or None of no confidence match found
+        Returns: (patch_id, DistroPatchMatch), or None of no confidence match found
         """
 
-        # define confidence weights
-        author_match = 0
+        # Define confidence weights
+        best_patch = None
+        best_confidence = 0.0
+        best_author_confidence = 0.0
+        best_subject_confidence = 0.0
+        best_description_confidence = 0.0
+        best_filenames_confidence = 0.0
+
+        # Confidence weights
+        author_weight = 0.3
+        subject_weight = 0.3
+        description_weight = 0.1
+        filenames_weight = 0.3
+
+        # Threshold that we must hit to return a match
+        threshold = 0.0
+
+        for patch_id, patch in self.upstream_patches.items():
+            # Calculate confidence that our downstream patch matches this upstream patch
+
+            # Calculate filenames confidence, which is the percentage of files upstream that are present in the downstream patch
+            num_filenames_match = 0
+            upstream_filenames = tuple(patch.filenames)
+            for downstream_filename in downstream_patch.filenames:
+                if (downstream_filename.endswith(upstream_filenames)):
+                    num_filenames_match += 1
+            filenames_confidence = float(num_filenames_match) / len(patch.filenames)
+
+            author_confidence = fuzz.partial_ratio(patch.author_name, downstream_patch.author_name) / 100.0
+            subject_confidence = fuzz.partial_ratio(patch.subject, downstream_patch.subject) / 100.0
+            # Temporarily for description only checking exact string is in
+            description_confidence = 1.0 if patch.description in downstream_patch.description else 0.0
+
+            confidence = author_weight*author_confidence + subject_weight*subject_confidence + description_weight*description_confidence + filenames_weight*filenames_confidence
+            if confidence > best_confidence:
+                best_patch = patch
+                best_confidence = confidence
+                best_author_confidence = author_confidence
+                best_subject_confidence = subject_confidence
+                best_description_confidence = description_confidence
+                best_filenames_confidence = filenames_confidence
+            elif (confidence == best_confidence):
+                print("[info] Two patches found with same confidence")
+
+        if best_confidence < threshold:
+            return None
+
+        return (patch_id, DistroPatchMatch(best_author_confidence, best_subject_confidence, best_description_confidence, best_filenames_confidence, best_confidence, best_patch))
 
 
-
-        best_match_patch = None
-        best_match_conf = 0.0
-        best_match_author_conf = 0.0
-        best_match_subject_conf = 0.0
-        best_match_description_conf = 0.0
-        best_match_filenames_conf = 0.0
-
-        # for patch in upstream_patches:
-            # if 
-
-        return None
 
 
 
--- Objects/Patch.py
+++ Objects/Patch.py
@@ -2,11 +2,10 @@
 class Patch:
     def __init__(self, subject, author_name, author_email, upstream, description, commit_date, filenames, patch_id = ""):
         self.subject = subject
-        self.commit_id = commit_id
         self.author_name = author_name
-        self.author_email = email
+        self.author_email = author_email
         self.upstream = upstream
         self.description = description
         self.commit_date = commit_date
-        self.filenames = filesnames
+        self.filenames = filenames
         self.patch_id = ""
