fb316a90caaebe84f0014ef683e45f3f89d45715
Abhishek Marathe <abmarath@microsoft.com>
2019-09-24 14:48:55 -0700

    exception handling and single branch pulling

--- DownstreamTracker/MonitorUbuntu.py
+++ DownstreamTracker/MonitorUbuntu.py
@@ -16,42 +16,49 @@ from DatabaseDriver.DistroTable import DistroTable
 from UpstreamTracker.ParseData import parse_log
 
 def monitor_distro(distro):
-    # make sure that Kernel is present
-    print(distro.distro_id+" Monitoring Script..")
-    folder_name = distro.repo_link.rsplit('/', 1)[-1]
-    print(folder_name)
-    if os.path.exists(cst.PathToClone+folder_name):
-        print("[Info] Path to Ubuntu Bionic Repo exists")
-        repo = git.Repo(cst.PathToClone+folder_name)
-        print("[Info] Pulling recent changes")
-        repo.remotes.origin.pull()
-        print("[Info] Git pull complete")
-    else:
-        print("[Info] Path to Ubuntu Bionic does not exists")
-        print("[Info] Cloning Ubuntu Bionic repo")
-        git.Git(cst.PathToClone).clone(distro.repo_link)
-        print("[Info] Cloning Complete")
+    try:
+        # make sure that Kernel is present
+        print(distro.distro_id+" Monitoring Script..")
+        folder_name = distro.repo_link.rsplit('/', 1)[-1]
+        print(folder_name)
+        if os.path.exists(cst.PathToClone+folder_name):
+            print("[Info] Path to Ubuntu Bionic Repo exists")
+            repo = git.Repo(cst.PathToClone+folder_name)
+            print("[Info] Pulling recent changes")
+            repo.remotes.origin.pull()
+            print("[Info] Git pull complete")
+        else:
+            print("[Info] Path to Ubuntu Bionic does not exists")
+            print("[Info] Cloning Ubuntu Bionic repo")
+            # clone single branch
+            #git.Git(cst.PathToClone).clone(distro.repo_link)
+            git.Repo.clone_from(distro.repo_link,cst.PathToClone+folder_name,branch='master')
+            print("[Info] Cloning Complete")
 
 
-    #parse maintainers file to get hyperV files
-    print("[Info] parsing maintainers files")
-    fileList = parseMaintainers(cst.PathToClone+folder_name)
-    print("[Info] Received HyperV file paths")
-    fileNames = sanitizeFileNames(fileList)
+        #parse maintainers file to get hyperV files
+        print("[Info] parsing maintainers files")
+        fileList = parseMaintainers(cst.PathToClone+folder_name)
+        print("[Info] Received HyperV file paths")
+        fileNames = sanitizeFileNames(fileList)
 
 
-    # Collecting git logs for HyperV files
-    print("[Info] Preprocessed HyperV file paths")
-    currDir = os.getcwd()
-    os.chdir(cst.PathToClone+folder_name)
-    command = "git log -p -- "+' '.join(fileNames)+" > "+cst.PathToCommitLog+"/"+folder_name+"Log"
-    os.system(command)
-    print("[Info] Created HyperV files git logs at "+cst.PathToCommitLog+"/"+folder_name+"Log")
+        # Collecting git logs for HyperV files
+        print("[Info] Preprocessed HyperV file paths")
+        currDir = os.getcwd()
+        os.chdir(cst.PathToClone+folder_name)
+        command = "git log -p -- "+' '.join(fileNames)+" > "+cst.PathToCommitLog+"/"+folder_name+"Log"
+        os.system(command)
+        print("[Info] Created HyperV files git logs at "+cst.PathToCommitLog+"/"+folder_name+"Log")
 
-    # Parse git log and dump data into database
-    match = DownstreamMatcher(UpstreamPatchTable())
-    parse_log(cst.PathToCommitLog+"/"+folder_name+"Log", DistroMatch(), match, distro, distro.distro_id)
-    os.chdir(currDir)
+        # Parse git log and dump data into database
+        match = DownstreamMatcher(UpstreamPatchTable())
+        parse_log(cst.PathToCommitLog+"/"+folder_name+"Log", DistroMatch(), match, distro, distro.distro_id)
+        os.chdir(currDir)
+    except Exception:
+        print("[Error] Exception occured "+Exception)
+    finally:
+        print("[Info] End of parsing for "+distro.distro_id)
 
 if __name__ == '__main__':
     print("Welcome to Patch tracker!!")
