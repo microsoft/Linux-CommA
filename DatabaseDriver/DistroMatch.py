49674257461c0ccabbe359c65bd859fa26ef76ea
Ryan Feldman <ryfeldma@microsoft.com>
2020-02-03 12:59:07 -0800

    Removing unneeded file and adding TODO for DB Optimization

--- DatabaseDriver/DistroMatch.py
+++ DatabaseDriver/DistroMatch.py
@@ -24,6 +24,7 @@ class DistroMatch():
         """
         Check if commit is already present in database
         """
+        # TODO change to count to optimize
         rows = self.cursor.execute(
             "SELECT * from [" + cst.DOWNSTREAM_TABLE_NAME + "] where commitId like ? and distroId like ? and kernelVersion like ?;",
             commit_id, distro.distro_id, distro.kernel_version).fetchall()
