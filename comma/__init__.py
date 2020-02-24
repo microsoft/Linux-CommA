e17be454847f4e0f03d118ea0a3faa6526e989b7
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-02-24 23:09:09 +0000

    Get files from diff

--- comma/__init__.py
+++ comma/__init__.py
@@ -16,7 +16,11 @@ repo = (
 #     print(commit.message)
 
 commit = repo.get("2e90ca68b0d2f5548804f22f0dd61145516171e3")
-print(commit.tree.diff_to_tree(commit.parents[0].tree, swap=True).patch)
+diff = commit.tree.diff_to_tree(commit.parents[0].tree, swap=True)
+# print(diff.patch)
+for d in diff.deltas:
+    print(d.new_file.path)
+    print(d.old_file.path)
 
 
 class Patch(Document):
