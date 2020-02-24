c608aea10689b56ac2f6e643c3bf99515152b406
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-02-24 22:50:29 +0000

    Basic commit diffing

--- comma/__init__.py
+++ comma/__init__.py
@@ -9,5 +9,11 @@ repo = (
     else clone_repository(repo_url, repo_path, bare=True)
 )
 
-for commit in repo.walk(repo.head.target):
-    print(commit.message)
+# for commit in repo.walk(
+#     pygit2.Oid(hex="63623fd44972d1ed2bfb6e0fb631dfcf547fd1e7"), pygit2.GIT_SORT_REVERSE
+# ):
+#     print(commit.message)
+
+commit = repo.get("2e90ca68b0d2f5548804f22f0dd61145516171e3")
+print(commit.tree.diff_to_tree(commit.parents[0].tree, swap=True).patch)
+
