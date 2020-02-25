28958bb6011c66fd8fc27b57f03c04f6c691c807
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-02-25 21:31:49 +0000

    Iterate over two repos but limit to since Hyper-V
    Temporarily ignore hunks for testing.

--- comma/__init__.py
+++ comma/__init__.py
@@ -1,64 +1,80 @@
 from datetime import datetime, timezone, timedelta
 from elasticsearch import Elasticsearch, helpers
-from pygit2 import Repository, discover_repository, clone_repository
-
-repo_url = "git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
-repo_path = "repos/linux-mainline"
-
-repo = (
-    Repository(repo_path)
-    if discover_repository(repo_path)
-    else clone_repository(repo_url, repo_path, bare=True)
+from pygit2 import (
+    Repository,
+    discover_repository,
+    clone_repository,
+    GIT_SORT_TOPOLOGICAL,
 )
 
 
 def get_patches():
-    for commit in repo.walk(repo.head.target):
-        author_time = datetime.fromtimestamp(
-            float(commit.author.time), timezone(timedelta(minutes=commit.author.offset))
-        ).isoformat()
-        commit_time = datetime.fromtimestamp(
-            float(commit.commit_time),
-            timezone(timedelta(minutes=commit.commit_time_offset)),
-        ).isoformat()
-        diff = repo.diff(commit.parents[0], commit)
-        files = [d.new_file.path for d in diff.deltas]
-        yield {
-            "_index": "commits",
-            "_type": "document",
-            "_id": commit.hex,
-            "doc": {
-                "repo": "linux-mainline",
-                "commit_id": commit.hex,
-                "parent_ids": [p.hex for p in commit.parents],
-                "author": {
-                    "name": commit.author.name,
-                    "email": commit.author.email,
-                    "time": author_time,
+    repos = [
+        (
+            "linux-mainline",
+            "git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git",
+            "/tmp/linux-mainline",
+        ),
+        ("openSUSE", "https://github.com/openSUSE/kernel.git", "/tmp/openSUSE"),
+    ]
+    for name, url, path in repos:
+        if discover_repository(path):
+            repo = Repository(path)
+            repo.remotes["origin"].fetch()
+        else:
+            repo = clone_repository(url, path, bare=True)
+
+        walker = repo.walk(repo.head.target, GIT_SORT_TOPOLOGICAL)
+        # First commit before introduction of Hyper-V code.
+        walker.hide(repo["578f2938a43f83988f6edab86cff487889b4ef58"].id)
+        for commit in walker:
+            author_time = datetime.fromtimestamp(
+                float(commit.author.time),
+                timezone(timedelta(minutes=commit.author.offset)),
+            ).isoformat()
+            commit_time = datetime.fromtimestamp(
+                float(commit.commit_time),
+                timezone(timedelta(minutes=commit.commit_time_offset)),
+            ).isoformat()
+            diff = repo.diff(commit.parents[0], commit)
+            files = [d.new_file.path for d in diff.deltas]
+            yield {
+                "_index": "commits",
+                "_type": "document",
+                "_id": commit.hex,
+                "doc": {
+                    "repo": name,
+                    "commit_id": commit.hex,
+                    "patch_id": diff.patchid.hex,
+                    "parent_ids": [p.hex for p in commit.parents],
+                    "author": {
+                        "name": commit.author.name,
+                        "email": commit.author.email,
+                        "time": author_time,
+                    },
+                    "committer": {
+                        "name": commit.committer.name,
+                        "email": commit.committer.email,
+                        "time": commit_time,
+                    },
+                    "summary": commit.message.splitlines()[0],
+                    # TODO: Split out Signed-off-by etc.
+                    "message": "\n".join(commit.message.splitlines()[2:]),
+                    "files": files,
+                    # "hunks": [
+                    #     {
+                    #         "header": h.header,
+                    #         "lines": [
+                    #             l.content.strip()
+                    #             for l in h.lines
+                    #             if not l.content.isspace()
+                    #         ],
+                    #     }
+                    #     for d in diff
+                    #     for h in d.hunks
+                    # ],
                 },
-                "committer": {
-                    "name": commit.committer.name,
-                    "email": commit.committer.email,
-                    "time": commit_time,
-                },
-                "summary": commit.message.splitlines()[0],
-                # TODO: Split out Signed-off-by etc.
-                "message": "\n".join(commit.message.splitlines()[2:]),
-                "files": files,
-                "hunks": [
-                    {
-                        "header": h.header,
-                        "lines": [
-                            l.content.strip()
-                            for l in h.lines
-                            if not l.content.isspace()
-                        ],
-                    }
-                    for d in diff
-                    for h in d.hunks
-                ],
-            },
-        }
+            }
 
 
 elastic = Elasticsearch(sniff_on_start=True)
