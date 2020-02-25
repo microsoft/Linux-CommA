c5ee59ecb7f739e2404bce8d0da42394a193b62c
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-02-25 04:47:38 +0000

    Index all commits

--- comma/__init__.py
+++ comma/__init__.py
@@ -11,43 +11,39 @@ repo = (
     else clone_repository(repo_url, repo_path, bare=True)
 )
 
-# for commit in repo.walk(
-#     pygit2.Oid(hex="63623fd44972d1ed2bfb6e0fb631dfcf547fd1e7"), pygit2.GIT_SORT_REVERSE
-# ):
-#     print(commit.message)
-
 
 def get_patches():
-    commit = repo.get("35a571346a94fb93b5b3b6a599675ef3384bc75c")
-    author_time = datetime.fromtimestamp(
-        float(commit.author.time), timezone(timedelta(minutes=commit.author.offset))
-    ).isoformat()
-    commit_time = datetime.fromtimestamp(
-        float(commit.commit_time),
-        timezone(timedelta(minutes=commit.commit_time_offset)),
-    ).isoformat()
-    diff = repo.diff(commit.parents[0], commit)
-    files = [d.new_file.path for d in diff.deltas]
-    yield {
-        "_index": "linux-mainline",
-        "_op_type": "index",
-        "_type": "document",
-        "_id": commit.hex,
-        "doc": {
-            "commit_id": commit.hex,
-            "author_name": commit.author.name,
-            "author_email": commit.author.email,
-            "author_time": author_time,
-            "committer_name": commit.committer.name,
-            "committer_email": commit.committer.email,
-            "commit_time": commit_time,
-            "message": commit.message,
-            "files": files,
-            "patch": diff,
-        },
-    }
+    for commit in repo.walk(repo.head.target):
+        author_time = datetime.fromtimestamp(
+            float(commit.author.time), timezone(timedelta(minutes=commit.author.offset))
+        ).isoformat()
+        commit_time = datetime.fromtimestamp(
+            float(commit.commit_time),
+            timezone(timedelta(minutes=commit.commit_time_offset)),
+        ).isoformat()
+        diff = repo.diff(commit.parents[0], commit)
+        files = [d.new_file.path for d in diff.deltas]
+        yield {
+            "_index": "commits",
+            "_type": "document",
+            "_id": commit.hex,
+            "doc": {
+                "repo": "linux-mainline",
+                "commit_id": commit.hex,
+                "author_name": commit.author.name,
+                "author_email": commit.author.email,
+                "author_time": author_time,
+                "committer_name": commit.committer.name,
+                "committer_email": commit.committer.email,
+                "commit_time": commit_time,
+                "message": commit.message,
+                "files": files,
+                "patch": diff.patch,
+            },
+        }
 
 
 es = Elasticsearch(sniff_on_start=True)
 print(es.info())
 Elasticsearch.bulk(es, get_patches())
+# print([p for p in get_patches()])
