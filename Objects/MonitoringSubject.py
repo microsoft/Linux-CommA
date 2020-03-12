bf6d666dae20895fec389dfcb10eb702f9b5c204
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-03-12 16:52:17 +0000

    Run `black` Python formatting tool on files
    As well as `dos2unix` for files with Windows line endings.

--- Objects/MonitoringSubject.py
+++ Objects/MonitoringSubject.py
@@ -1,4 +1,3 @@
-
 class MonitoringSubject:
     def __init__(self, monitoring_subject_id, distro_id, revision):
         self.monitoring_subject_id = monitoring_subject_id
--- Setup/DbCred.py
+++ Setup/DbCred.py
@@ -4,7 +4,8 @@ import os
 import git
 from dotenv import load_dotenv
 from pathlib import Path  # python3 only
-env_path = Path('.') / 'prod.env'
+
+env_path = Path(".") / "prod.env"
 load_dotenv(dotenv_path=env_path)
 
 
@@ -20,12 +21,21 @@ class DatabaseCredentials:
         else:
             print("[Info] Path to Secrets repo does not exists")
             print("[Info] Cloning Secrets repo")
-            db_cred_environ_name = 'LSG_SECRET_DB_CRED'
+            db_cred_environ_name = "LSG_SECRET_DB_CRED"
             db_cred = os.getenv(db_cred_environ_name)
-            if (db_cred is None):
-                print("[Error] Please set %s environment variable as your token to access LSG-Secret repo." % db_cred_environ_name)
-                raise Exception("LSG-Secret Repo token not set in environment variale: %s" % db_cred_environ_name)
-            git.Git(cst.PATH_TO_REPOS).clone("https://anything:%s@<redacted>" % db_cred)
+            if db_cred is None:
+                print(
+                    "[Error] Please set %s environment variable as your token to access LSG-Secret repo."
+                    % db_cred_environ_name
+                )
+                raise Exception(
+                    "LSG-Secret Repo token not set in environment variale: %s"
+                    % db_cred_environ_name
+                )
+            git.Git(cst.PATH_TO_REPOS).clone(
+                "https://anything:%s@<redacted>"
+                % db_cred
+            )
             print("[Info] Cloning Complete")
         tree = ET.parse(os.path.join(secret_repo_path, "PatchTrackerSecrets.xml"))
         root = tree.getroot()
