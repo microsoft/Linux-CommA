a1ebee7c175db669701fd38ddc1baf3ab638be81
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-05-20 15:35:56 -0700

    Delete now unused `Util.Constants`
    All the constants have been moved into single-definition generics.

--- Util/Constants.py
+++
@@ -1,9 +0,0 @@
-import platform
-
-PATH_TO_REPOS = "Repos"
-LINUX_REPO_URL = "https://github.com/torvalds/linux.git"
-LINUX_REPO_NAME = "linux.git"
-PATH_TO_LAST_SHA = "../lastSHA"
-LINUX_SYMBOL_REPO_NAME = "linux-sym"
-
-RedirectOp = ">>" if platform.system() == "Windows" else ">"
