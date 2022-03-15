fdbd72dcbaa576d6987d59a5f6bfc8fd5588b031
Matthew G. McGovern <mamcgove@microsoft.com>
2022-03-15 09:49:26 -0700

    remove github repo secrets requirement (#9)
    Repository scanning for plaintext creds is generating a lot of reports, also storing secrets in plaintext in a repository is probably not a great idea, so let's remove the requirement to keep your db creds in a repository.
    
    This PR:
    
    - rewrites the credentials class to just pull the secrets from environment variables.
    - changes documentation to reflect this and advise the user of the change.
    
    Azure Pipelines allows these to be set at runtime from an enciphered vault and also scrubs any instances of the secrets from the logs, the documentation now advises users to use a similar tool.

--- DatabaseDriver/Credentials.py
+++ DatabaseDriver/Credentials.py
@@ -1,41 +1,16 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-import logging
 import os
-import pathlib
-import sys
-import xml.etree.ElementTree
-
-import Util.Tracking
 
 
 class DatabaseCredentials:
     def __init__(self):
-        path = Util.Tracking.get_repo_path("secrets")
-
-        env_var = "COMMA_SECRETS_URL"
-        secrets_url = os.environ.get(env_var)
-        if not path.exists() and secrets_url is None:
-            logging.error(
-                f"Please set the environment variable '{env_var}' as the URL to clone your secrets repo."
-            )
-            sys.exit(1)
-
-        # NOTE: It is possible that `secrets_url` might be `None`, but
-        # at this point we're assuming it's already cloned and
-        # therefore just needs to be pulled. Also, it is possible the
-        # _server_ from which we're cloning does not support
-        # `--shallow-since`, so we have to disable this here.
-        Util.Tracking.get_repo(
-            name="secrets", url=secrets_url, shallow=False, pull=True
+        self.server = os.environ.get("COMMA_DB_URL")
+        self.name = os.environ.get("COMMA_DB_NAME")
+        self.user = os.environ.get("COMMA_DB_USERNAME")
+        self.password = os.environ.get("COMMA_DB_PW")
+        assert all([self.server, self.name, self.user, self.password]), (
+            "Missing a credential environment variable. Check that "
+            "COMMA_DB_URL, COMMA_DB_NAME, COMMA_DB_USERNAME and COMMA_DB_PW "
+            "are all set with the correct database info."
         )
-
-        root = xml.etree.ElementTree.parse(
-            # TODO: Rename XML file to e.g. `CommASecrets.xml`.
-            pathlib.Path(path, "PatchTrackerSecrets.xml").resolve()
-        ).getroot()
-
-        self.server = root.find("DatabaseServer").text
-        self.name = root.find("DatabaseName").text
-        self.user = root.find("DatabaseUser").text
-        self.password = root.find("DatabasePassword").text
